09866e4b938452be53872810411d96f4c327bb2b
Jamie Luong <jamieluong03@gmail.com>
2019-11-20 13:15:23 -0800

    update

--- models/medLog.js
+++ models/medLog.js
@@ -2,7 +2,10 @@ module.exports = function(sequelize, DataTypes) {
     var MedLog = sequelize.define("MedLog", {
         
         //medRoutine_ID (INT)
-
+        medRoutine_ID: {
+            type: DataTypes.INTEGER,
+            
+        }
         //Date (INT)
 
         //StartTime (INT)
