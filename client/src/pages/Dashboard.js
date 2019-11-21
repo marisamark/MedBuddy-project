3c6e6a0f31bb940b52e92e2c417637b61ac7c035
Marisa Mark <themarisamark@gmail.com>
2019-11-21 11:37:16 -0800

    adding basic component for my meds and adding medstaken and medsnottaken to dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NavAfterLogin from "../components/Nav (afterlogin)/index";
 import Calendarbar from "../components/Dashboard/Calendarbar";
+import Medstaken from "../components/Dashboard/Medstaken/index";
+import Medsnottaken from "../components/Dashboard/Medsnottaken/index";
 
 
 function Dashboard({}) {
@@ -8,6 +10,8 @@ function Dashboard({}) {
         <Container fluid>
         <NavAfterLogin></NavAfterLogin>
         <Calendarbar></Calendarbar>
+        <Medstaken></Medstaken>
+        <Medsnottaken></Medsnottaken>
     </Container>
     )
 }
--- client/src/pages/MyMedicatonspage.js
+++ client/src/pages/MyMedicatonspage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import NavAfterLogin from "../components/Nav (afterlogin)/index";
+import Meddata from "../components/My Medications/Med data/index";
 
 
 function MyMedications({}) {
     return (
      <Container fluid>
-        <NavAfterLogin>
-
-        </NavAfterLogin>
+        <NavAfterLogin></NavAfterLogin>
+        <Meddata></Meddata>
     </Container>
     )
 }
