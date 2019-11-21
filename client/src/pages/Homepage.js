b503569bee56e7c438c33bda47c6021a8a21c53c
Marisa Mark <themarisamark@gmail.com>
2019-11-21 13:20:59 -0800

    adding jumbotron element to home

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -3,12 +3,13 @@ import NavBeforeLogin from "../components/Home/NavBar (public)/index"
 import GoodRxwidget from "../components/Home/GoodRX widget/index";
 import InfoCards from "../components/Home/Information cards/index";
 import LoginForm from "../components/Home/Login Form/index"
+import Jumbotron from "../components/Home/Jumbotron/index"
 
-
-const Home = () => {
+function Home() {
     return (
         <Container fluid>
             <NavBeforeLogin></NavBeforeLogin>
+            <Jumbotron></Jumbotron>
             <GoodRxwidget></GoodRxwidget>
             <InfoCards></InfoCards>
             <LoginForm></LoginForm>
