2b87d99d83af56a6f63a0e9bb848d1e83b24296a
Marisa Mark <themarisamark@gmail.com>
2019-11-21 11:22:11 -0800

    adding basic dashboard components

--- client/src/components/Dashboard/Medsnottaken/index.js
+++ client/src/components/Dashboard/Medsnottaken/index.js
@@ -1,2 +1,23 @@
-//container
-//cards of meds not taken
+// //container
+// //cards of meds not taken
+// <div class="container">
+//   <div class="row">
+//     <div class="col">
+//       1 of 2
+//     </div>
+//     <div class="col">
+//       2 of 2
+//     </div>
+//   </div>
+
+import React from "react";
+
+function Medsnottaken(){
+    return(
+        <div class="col">
+            <h3 class="text-center">Medications I have to take</h3>
+            <div id="medsnottaken"></div>
+        </div>
+
+    )
+}
--- client/src/components/Dashboard/Medstaken/index.js
+++ client/src/components/Dashboard/Medstaken/index.js
@@ -1,2 +1,17 @@
 //container
 //cards of meds taken
+
+
+import React from "react";
+
+function Medstaken(){
+    return(
+        <div class="col">
+            <h3 class="text-center">Medications I've taken</h3>
+            <div id="medstaken"></div>
+        </div>
+
+    )
+}
+
+export default Medstaken;
