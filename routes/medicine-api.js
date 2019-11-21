a902d2d19d6091e3d4ea902188f19584b79aac4e
anthony soon <anthonychansoon@gmail.com>
2019-11-21 10:27:33 -0800

    medicine api

---
+++ routes/medicine-api.js
@@ -0,0 +1,22 @@
+var db = require("../models");
+
+module.exports = function (app) {
+    // GET medicine
+    app.get("/api/medicine", function (req, res) {
+        db.Medicine.findAll({})
+            .then(function (dbmedicine) {
+                res.json(dbmedicine);
+            })
+    });
+    // POST medicine
+    // need to check req.body
+    app.post("/api/medicine", function (req, res) {
+        console.log(req.body);
+        db.Medicine.create({
+            medicinename : req.body
+        })
+        .then(function(dbmedicine){
+            res.json(dbmedicine);
+        })
+    });
+}
