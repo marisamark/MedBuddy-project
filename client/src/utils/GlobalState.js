f9cbe140c8b7987b69398377ac11404b57921650
anthony soon <anthonychansoon@gmail.com>
2019-11-22 11:07:07 -0800

    globalstate

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -22,5 +22,40 @@
 // 	}]
 
 import React, { createContext, useReducer, useContext } from "react";
+
+
 const StoreContext = createContext();
 const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props}) => {
+    const [state, dispatch] = useReducer(reducer, {
+       user: {
+        id: 0,
+        	user: "",
+        	password: "",
+        	firstName: "",
+        	lastName: ""
+       },
+       medroutine: [{
+           id: 0,
+           datecount: 0,
+           dosage : 0,
+           dose : 0,
+           userid : 0,
+           medlog : [{ 
+               id: 0,
+               date : Date.now(),
+               time : 0,
+               status : false,
+               medroutineid : 0
+           }]
+       }]
+    })
+    return < Provider value = {[state, dispatch]} {...props} />;
+}
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+}
+
+export {StoreProvider, useStoreContext};
