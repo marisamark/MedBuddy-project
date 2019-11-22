f9450c3fb961b7dc12e33c78d9bce73627c51f48
Marisa Mark <themarisamark@gmail.com>
2019-11-22 12:44:55 -0800

    adding dashboard tab to navbar after login

--- client/src/components/Nav (afterlogin)/index.js
+++ client/src/components/Nav (afterlogin)/index.js
@@ -6,6 +6,7 @@ function NavAfterLogin() {
 //add logo later
   return (
 <nav class="nav justify-content-center">
+<a class="nav-link active" href="/dashboard">Dashboard</a>
   <a class="nav-link active" href="/mymedications">My Medications</a>
   <a class="nav-link active" href="/addmedications">Add Medications</a>
   <a class="nav-link" href="/home">Logout</a>
