5a1c9fe19c5f89fbc668e951e2fb3a26951166a3
Emi Rezaei <tomoyose.emi@gmail.com>
2019-11-22 12:26:46 -0800

    fixed navbar

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBeforeLogin from "../components/Nav (afterlogin)/index";
+import NavBeforeLogin from "../components/Home/NavBar (public)/index"
 import SignupForm from "../components/Home/Signup form/index";
 
 
