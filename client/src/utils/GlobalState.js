5c8990588ec327b91f012fa794b00d621eaa75b1
anthony soon <anthonychansoon@gmail.com>
2019-11-24 12:31:34 -0800

    not yet working

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,31 +1,10 @@
-// STATE
-
-// user: 	{
-// 	id: 0,
-// 	user: "",
-// 	password: "",
-// 	firstName: "",
-// 	lastName: ""
-// 	},
-// medRoutine: [{
-// 	id: 0,
-// 	dose: "1 tab",
-// 	medLog: [{
-// 		id: 0,
-// 		date: new Date().getMonth() + "/" + new Date().getDate() + "/" + new Date().getFullYear(),
-// 		time: new Date().getHours() + ":" + new Date().getMinutes(),
-// 		status: false,
-// 		}],
-// 	medicine: {
-// 		name: "aspirin",
-// 		}
-// 	}]
-
 import React, { createContext, useReducer, useContext } from "react";
 
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
+
+
 
 const StoreProvider = ({ value = [], ...props}) => {
     const [state, dispatch] = useReducer(reducer, {
