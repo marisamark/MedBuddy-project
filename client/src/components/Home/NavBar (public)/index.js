d201b0a2e8bb7c4ccaa33d1af6193142777b69d1
Marisa Mark <themarisamark@gmail.com>
2019-11-25 11:36:28 -0800

    public navbar now uses react

--- client/src/components/Home/NavBar (public)/index.js
+++ client/src/components/Home/NavBar (public)/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 // import { useStoreContext } from "../../utils/GlobalState";
-
+import { Navbar, Nav, Button } from 'react-bootstrap';
 function NavBeforeLogin() {
 
   return (
-<nav className="nav ml-2 mt-2 mr-2">
-<a className="ml-2 mt-2 mr-2 mb-2" href="/home"><img height="60px" width="120px" src="bluemedbuddy.png" alt="logo"/>
-</a>
-<a className="nav-link active mt-3" href="/home">Home</a>
-  <a className="nav-link active mt-3" href="/health">Health and Wellness</a>
-  <a className="nav-link mt-3 float-left" href="/signup">Signup</a>
-</nav>
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="/home"><img height="60px" width="120px" src="bluemedbuddy.png" alt="logo"/></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/health">Health and Wellness</Nav.Link>
+      <Nav.Link href="/signup">Signup</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
   );
 }
 
