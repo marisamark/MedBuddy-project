e5068502f8c34bcb39f65930d0aac9b1e204216c
Jamie Luong <jamieluong03@gmail.com>
2019-11-25 02:03:33 -0800

    cleaned up some codes

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,28 +1,8 @@
-// STATE
 
-// user: 	{
-// 	id: 0,
-// 	user: "",
-// 	password: "",
-// 	firstName: "",
-// 	lastName: ""
-// 	},
-// medRoutine: [{
-// 	id: 0,
-// 	dose: "1 tab",
-// 	medLog: [{
-// 		id: 0,
 // 		date: new Date().getMonth() + "/" + new Date().getDate() + "/" + new Date().getFullYear(),
 // 		time: new Date().getHours() + ":" + new Date().getMinutes(),
-// 		status: false,
-// 		}],
-// 	medicine: {
-// 		name: "aspirin",
-// 		}
-// 	}]
 
 import React, { createContext, useReducer, useContext } from "react";
-
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
