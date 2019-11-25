9e3609dfd0a1c00db13888e8e1690d5c20231361
Marisa Mark <themarisamark@gmail.com>
2019-11-25 13:14:08 -0800

    centering cards on dashboard and making them responsive

--- client/src/components/Dashboard/MedsCard/index.js
+++ client/src/components/Dashboard/MedsCard/index.js
@@ -7,25 +7,37 @@ function MedsCard(){
     return(
         
        
-        <Card style={{ width: '18rem' ,'marginLeft': '140px' }}>
-        <Card.Body>
-          <Card.Title>Time</Card.Title>
+      //   <Card class="card card-block d-flex">
+      //   <Card.Body>
+      //     <Card.Title>Time</Card.Title>
          
-            <ul>
-                <li>Name of Drug : </li>
-                <li>rug Concentration : </li>
-                <li>Dosage : </li>
-            </ul>
+      //       <ul>
+      //           <li>Name of Drug : </li>
+      //           <li>rug Concentration : </li>
+      //           <li>Dosage : </li>
+      //       </ul>
         
-          <div className="row">
-          <Button variant="primary" >Complete</Button>
+      //     <div className="row">
+      //     <Button variant="primary" >Complete</Button>
+      //     <Reschedule />
+      //     </div>
+      //   </Card.Body>
+      // </Card>
+      <div class="container">
+      <h2 className="text-center mt-3 mb-3">Medications I have to take</h2>
+          <div className="card mt-4">
+          <div className="card-body">
+            <h3 className="card-title text-center">Time</h3>
+                <h5 class="text-center">Name of Drug: </h5>
+                <h5 class="text-center">Drug Concentration: </h5> 
+                <h5 class="text-center">Dosage: </h5>
+            <div className="row justify-content-center">
+          <Button variant="primary">Complete</Button>
           <Reschedule />
           </div>
-        </Card.Body>
-      </Card>
-          
-    
-
+          </div>
+        </div>
+</div>
     )
 }
 
--- client/src/components/Dashboard/Medstaken/index.js
+++ client/src/components/Dashboard/Medstaken/index.js
@@ -7,12 +7,12 @@ import MedsTakenCard from "../MedsTakenCard/index"
 
 function Medstaken(){
     return(
-        <div className="col-6">
-            <h3 className="text-center">Medications I've taken</h3>
+<>
+            
             <div id="medstaken">
                 <MedsTakenCard />
             </div>
-        </div>
+</>
 
     )
 }
