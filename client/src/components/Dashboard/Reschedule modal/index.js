daf7bd6db5d319b29c0f7c2b83db2eac1240309e
Emi Rezaei <tomoyose.emi@gmail.com>
2019-11-24 23:11:38 -0800

    fixed modal

--- client/src/components/Dashboard/Reschedule modal/index.js
+++ client/src/components/Dashboard/Reschedule modal/index.js
@@ -1,33 +1,82 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+// require("react-bootstrap/ModalHeader")
 
-function Reschedule(){
-    return(
-        <div>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-    Reschedule
-  </button>
+function RescheduleModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Reschedule Time
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>What time would you like to reschedule?</h4>
+          <div class="row">
+                <div class="col-md-4">
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+                        <option>9</option>
+                        <option>10</option>
+                        <option>11</option>
+                        <option>12</option>
+                    </select>
+                    </div>
+                    <div class="col-md-4">
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>00</option>
+                        <option>15</option>
+                        <option>30</option>
+                        <option>45</option>
+                    </select>
+                    </div>
+                    <div class="col-md-4">
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>AM</option>
+                        <option>PM</option>
+                    </select>
+                    </div>
+                    <br />
+                    </div>
+                     {/* <div>{TimesDives}</div> */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Reschedule</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
   
- 
-  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">What Time?</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Reschedule</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
-    )
-}
-export default Reschedule;
+  function Reschedule() {
+    const [modalShow, setModalShow] = React.useState(false);
+  
+    return (
+      <ButtonToolbar>
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+          Reschedule
+        </Button>
+  
+        <RescheduleModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+      </ButtonToolbar>
+    );
+  }
+  
+  export default Reschedule;
