34df51509f96a424b1c6b422e985e8c6dae33eea
Emi Rezaei <tomoyose.emi@gmail.com>
2019-11-25 19:42:16 -0800

    added styling

--- client/src/components/Add Medications/Toast/index.js
+++ client/src/components/Add Medications/Toast/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Toast, Button, } from "react-bootstrap";
+import {Toast, Button, Alert } from "react-bootstrap";
 
 
 function ToastMeds() {
@@ -8,22 +8,26 @@ function ToastMeds() {
     return (
         <>
 
-          <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide style={{
-             position: 'fixed',
-             top: 0,
-             right: 0,
+          <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide 
+            style={{
+                position: 'fixed',
+                top: 0,
+                right: 0,
+                variant: "success"
            }}>
    
-        
-            <Toast.Header>
-              <strong className="mr-auto">Add Medicatoin</strong>
-              
-            </Toast.Header>
-            <Toast.Body>Added Your Medication to Dashboard</Toast.Body>
+                <Alert variant="success">
+                {/* <Toast.Header>
+                
+                <strong className="mr-auto">Add Medicatoin</strong>
+                
+                </Toast.Header> */}
+                <Toast.Body>Added Your Medication to Dashboard</Toast.Body>
+                </Alert>
           </Toast>
         
           <Button onClick={() => setShow(true)}>Add</Button>
-         
+          
           </>
     );
   }
