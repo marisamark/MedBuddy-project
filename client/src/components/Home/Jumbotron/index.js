0d69570ada727151c8dfb6365c69bf75b8ef139f
Emi Rezaei <tomoyose.emi@gmail.com>
2019-11-26 12:11:01 -0800

    added pic

--- client/src/components/Home/Jumbotron/index.js
+++ client/src/components/Home/Jumbotron/index.js
@@ -3,8 +3,10 @@ import LoginForm from "../Login Form";
 
 function Jumbotron() {
     return (
-        <div className="jumbotron jumbotron-fluid">
-            <div className="container">
+        <div className="jumbotron jumbotron-fluid bg-img">
+           <section className="hero is-large is-widescreen">
+            <div className="container hero-body">
+                
                 <h1 className="display-4">Welcome to Medbuddy</h1>
                 <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its
                 parent.</p>
@@ -16,6 +18,7 @@ function Jumbotron() {
                     </div>
                     </div>
             </div>
+            </section>
         </div>
 
     )
