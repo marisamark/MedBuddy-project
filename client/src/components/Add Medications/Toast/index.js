f2d0637d762ed748d426d026dd84715309363972
Emi Rezaei <tomoyose.emi@gmail.com>
2019-11-25 19:07:38 -0800

    fixed toast

--- client/src/components/Add Medications/Toast/index.js
+++ client/src/components/Add Medications/Toast/index.js
@@ -7,16 +7,9 @@ function ToastMeds() {
   
     return (
         <>
-        <div
-  aria-live="polite"
-  aria-atomic="true"
-  style={{
-    position: 'relative',
-    minHeight: '500px',
-  }}
->
+
           <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide style={{
-             position: 'relative',
+             position: 'fixed',
              top: 0,
              right: 0,
            }}>
@@ -24,13 +17,13 @@ function ToastMeds() {
         
             <Toast.Header>
               <strong className="mr-auto">Add Medicatoin</strong>
-              <small>11 mins ago</small>
+              
             </Toast.Header>
             <Toast.Body>Added Your Medication to Dashboard</Toast.Body>
           </Toast>
         
           <Button onClick={() => setShow(true)}>Add</Button>
-          </div>
+         
           </>
     );
   }
