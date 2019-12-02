72ec3830f26c9635473cc3f6e6df70e72d4185ff
Marisa Mark <themarisamark@gmail.com>
2019-12-02 11:59:08 -0800

    putting jumbotron elements in a col

--- client/src/components/Home/Jumbotron/index.js
+++ client/src/components/Home/Jumbotron/index.js
@@ -4,19 +4,24 @@ import LoginForm from "../Login Form";
 function Jumbotron() {
     return (
         <div id="jumbotron" className="jumbotron jumbotron-fluid bg-img">
-           {/* <section className="hero is-small is-widescreen"> */}
-            <div className="container welcome1">
-                <h1 id="welcometo" className="medbuddy font-weight-bold mr-4">Welcome to</h1>
-                <h1 id ="medbuddy" className="mr-4 medbuddy font-weight-bold ">MedBuddy</h1>    
-            </div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-11">
+            {/* <section className="hero is-small is-widescreen"> */}
+            <div className="container ">
+                <div className="col-lg-6 col-sm-12 col-md-6">
+                    <div class="row justify-content-center">
+                        <h1 id="welcometo" className="medbuddy font-weight-bold mr-4">Welcome to</h1>
+                        <h1 id="medbuddy" className="mr-4 medbuddy font-weight-bold ">MedBuddy</h1>
+
+                    </div>
+                    <div className="row justify-content-center">
                             <LoginForm />
-                        </div>
                     </div>
-                    </div>
-        
+
+                </div>
+                {/* <div className="container">
+
+                </div> */}
+            </div>
+
             {/* </section> */}
         </div>
 
