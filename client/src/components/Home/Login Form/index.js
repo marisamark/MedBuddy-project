55dd9339d9010c6ff716e1bb13929b7a02084cbb
anthony soon <anthonychansoon@gmail.com>
2019-12-01 19:16:46 -0800

    trying

--- client/src/components/Home/Login Form/index.js
+++ client/src/components/Home/Login Form/index.js
@@ -4,6 +4,10 @@ import axios from "axios";
 import { GRAB_USER_INFO, GRAB_USER_ROUTINE } from "../../../utils/actions";
 import { StoreContext } from "../../../utils/GlobalState";
 import { push } from 'react-router-redux';
+import { Redirect } from "react-router-dom";
+import history from "../../../App"
+import Dashboard from "../../../pages/Dashboard";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 function LoginForm() {
 
@@ -57,34 +61,39 @@ function LoginForm() {
                 console.log(results.data)
                 routineid[i].medlog = results.data
                 // console.log("myroutine", myroutine)
-                if (i === routineid.length - 1){
-                    dispatch({ type: GRAB_USER_ROUTINE, payload:routineid}) 
-                    // window.location.replace("/dashboard");
+                if (i === routineid.length - 1) {
+                    dispatch({ type: GRAB_USER_ROUTINE, payload: routineid })
+                    window.location.replace("/dashboard");
                     // props.history.push('/dashboard')
                     // push('/dashboard')
                     //history.replace('/dashboard')
+                    // dispatch(push('/dashboard'))
+                    <Router>
+
+                        
+                    </Router>
                 }
-            }) 
+            })
         }
-       
+
     }
 
     return (
         <div className="card float-right mr-4 ml-4 mt-4 mb-4 text-color">
             <div className="card-body">
-            <form className="float-right mb-4">
-                <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your username..." ref={username}>
-                    </input>
-                </div>
+                <form className="float-right mb-4">
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your username..." ref={username}>
+                        </input>
+                    </div>
 
-                <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Your password..." ref={password}></input>
-                </div>
-                <button type="submit" id="button-blue" className="btn btn-light text-light" onClick={loginClick}>Log In</button>
-            </form>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Your password..." ref={password}></input>
+                    </div>
+                    <button type="submit" id="button-blue" className="btn btn-light text-light" onClick={loginClick}>Log In</button>
+                </form>
             </div>
         </div>
     )
