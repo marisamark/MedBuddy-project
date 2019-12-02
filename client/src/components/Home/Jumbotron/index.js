7804fef22e0ccdb57ca330d499082fe1d0f5a482
Emi Rezaei <tomoyose.emi@gmail.com>
2019-12-02 12:42:59 -0800

    fixed welcom

--- client/src/components/Home/Jumbotron/index.js
+++ client/src/components/Home/Jumbotron/index.js
@@ -8,13 +8,16 @@ function Jumbotron() {
             <div className="container ">
                 <div className="col-lg-6 col-sm-12 col-md-6">
                     <div class="row justify-content-center">
-                        <h1 id="welcometo" className="medbuddy font-weight-bold mr-4">Welcome to</h1>
-                        <h1 id="medbuddy" className="mr-4 medbuddy font-weight-bold ">MedBuddy</h1>
-
+                        {/* <div> */}
+                            <h1 id="welcometo" className="medbuddy font-weight-bold mr-4">Welcome to</h1>
+                        </div>
+                        <div> 
+                            <h1 id="medbuddy" className="mr-4 medbuddy font-weight-bold ">MedBuddy</h1>
+                        {/* </div> */}
                     </div>
-                    <div className="row justify-content-center">
-                            <LoginForm />
-                    </div>
+                <div className="row justify-content-center">
+                        <LoginForm />
+                </div>
 
                 </div>
                 {/* <div className="container">
