fd12a4a0cf7b2cfeb1e7d6e0f65a7d1b56c267cd
Marisa Mark <themarisamark@gmail.com>
2019-12-02 11:37:28 -0800

    changing mobile background

--- client/src/components/Home/Jumbotron/index.js
+++ client/src/components/Home/Jumbotron/index.js
@@ -3,11 +3,11 @@ import LoginForm from "../Login Form";
 
 function Jumbotron() {
     return (
-        <div className="jumbotron jumbotron-fluid bg-img">
+        <div id="jumbotron" className="jumbotron jumbotron-fluid bg-img">
            {/* <section className="hero is-small is-widescreen"> */}
             <div className="container welcome1">
-                <h1 id="welcometo" className="medbuddy font-weight-bold text-white mr-4">Welcome to</h1>
-                <h1 id ="medbuddy" className="mr-4 medbuddy font-weight-bold text-white">MedBuddy</h1>    
+                <h1 id="welcometo" className="medbuddy font-weight-bold mr-4">Welcome to</h1>
+                <h1 id ="medbuddy" className="mr-4 medbuddy font-weight-bold ">MedBuddy</h1>    
             </div>
                 <div className="container">
                     <div className="row">
