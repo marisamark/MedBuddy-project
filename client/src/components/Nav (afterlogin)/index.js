279b23fc4929793e9dc9e59a1ebe0c0267eb2334
anthony soon <anthonychansoon@gmail.com>
2019-12-02 13:08:46 -0800

    zzz

--- client/src/components/Nav (afterlogin)/index.js
+++ client/src/components/Nav (afterlogin)/index.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 // import { useStoreContext } from "../../utils/GlobalState";
 import { Navbar, Nav, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import {Link} from 'react-router-dom'
+import {StoreContext} from "../../utils/GlobalState"
 
 function NavAfterLogin() {
+
   return (
-<Navbar  className="nav-color" expand="lg">
-  <Navbar.Brand href="/dashboard"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo"/></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link className="text-light" href="/dashboard">Dashboard</Nav.Link>
-      <Nav.Link className="text-light" href="/mymedications">My Medications</Nav.Link>
-      <Nav.Link className="text-light" href="/addmedications">Add Medications</Nav.Link>    
-      <Nav.Link className="text-light" href="/home">Logout</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-  );
+
+    // loggedState ? (
+
+      <Navbar className="nav-color" expand="lg">
+        <Navbar.Brand href="/dashboard"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+
+            {/* <Link to ="/mymedications">My Medications</Link> */}
+            {/* <Link to ="/addmedications">Add Medications</Link> */}
+            <Nav.Link className="text-light" href="/dashboard">Dashboard</Nav.Link>
+            <Nav.Link className="text-light" href="/mymedications" >My Medications</Nav.Link>
+            <Nav.Link className="text-light" href="/addmedications">Add Medications</Nav.Link>
+            <Nav.Link className="text-light" href="/home">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+    ) 
+  //     <h1>not logged in</h1>
+  // )
 }
 
 export default NavAfterLogin;
