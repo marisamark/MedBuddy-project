0bca2e85831d887cb27c51509eea00181c940483
Emi Rezaei <tomoyose.emi@gmail.com>
2019-12-03 10:27:48 -0800

    changed nav

--- client/src/components/Nav (afterlogin)/index.js
+++ client/src/components/Nav (afterlogin)/index.js
@@ -14,28 +14,28 @@ function NavAfterLogin() {
 
   return (
 
-
-    <div>
-      <ul>
-        <li><Link to = "/dashboard">Dashboard</Link></li>
-        <li><Link to = "/mymedications">My Medications</Link></li>
-        <li><Link to = "/addmedications">Add Medications</Link></li>
-        <li><Link to = "/home">Logout</Link></li>
-      </ul>
-    </div>
+    // <div>
+    //   <ul>
+    //     <li><Link to = "/dashboard">Dashboard</Link></li>
+    //     <li><Link to = "/mymedications">My Medications</Link></li>
+    //     <li><Link to = "/addmedications">Add Medications</Link></li>
+    //     <li><Link to = "/home">Logout</Link></li>
+    //   </ul>
+    // </div>
+    <Navbar  className="nav-color" expand="lg">
+  <Navbar.Brand href="/dashboard" className="col"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo"/></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto" id="nav-loging"> 
+        <Link to = "/dashboard" className="text-light col">Dashboard</Link>   
+        <Link to ="/mymedications" className="text-light col-md-auto">My Medications</Link>  
+        <Link to = "/addmedications"className="text-light col-md-auto" >Add Medications</Link>               
+    </Nav>
+    <Link to = "/home" className="text-light" id="logout">Logout</Link> 
+  </Navbar.Collapse> 
+     
+</Navbar>
     
-    // <Navbar className="nav-color" expand="lg">
-    //   <Navbar.Brand href="/dashboard"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo" /></Navbar.Brand>
-    //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //   <Navbar.Collapse id="basic-navbar-nav">
-    //     <Nav className="mr-auto">
-    //       <Nav.Link className="text-light" href="/#dashboard">Dashboard</Nav.Link>
-    //       <Nav.Link className="text-light" href="/#mymedications" >My Medications</Nav.Link>
-    //       <Nav.Link className="text-light" href="/#addmedications">Add Medications</Nav.Link>
-    //       <Nav.Link className="text-light" href="/#home">Logout</Nav.Link>
-    //     </Nav>
-    //   </Navbar.Collapse>
-    // </Navbar>
   )
 
 
