ffe0b55f3a32f04930284db69cc210a2753a22d7
anthony soon <anthonychansoon@gmail.com>
2019-12-03 09:33:43 -0800

    links are working

--- client/src/components/Add Medications/Add Meds Form/index.js
+++ client/src/components/Add Medications/Add Meds Form/index.js
@@ -19,6 +19,7 @@ function MedicationForm() {
     const log = useRef();
 
     const [state, dispatch] = useStoreContext();
+    const [timeState, setTimeState] = useState({});
 
 
     console.log("initial " + userState.selectValue)
@@ -26,10 +27,29 @@ function MedicationForm() {
         setUserState({ selectValue: e.target.value });
     }
 
-    const handleSubmit = e => {
+    function handleSelect(data) {
+        console.log("SELECTING...")
+setTimeState({...timeState, [data.key]:data.val})
+    // function handleSelectMinute(e) {
+    //     console.log("SELECTING...")
+    //     console.log(e.target.value + "minutes")
+    //     setTimeState({...timeState, [e.target.id]:e.target.value})
+    // }
+    // function handleSelectAMPM(e) {
+    //     console.log("SELECTING...")
+    //     console.log(e.target.value)
+    // }
+    console.log(timeState)
+    }
+
+    const handleSubmit = (e, data) => {
         e.preventDefault();
         console.log('DOSAGE', dosage.current.value)
-        API.postMedicine({
+        submitAPI()
+    }
+
+    const submitAPI = ()=>{
+         API.postMedicine({
             medicinename: medicinename.current.value
         }).then(result => {
             console.log("POSTMEDICINE", result)
@@ -56,7 +76,6 @@ function MedicationForm() {
 
         })
         .catch(err => console.log(err));
-
     }
 
 
@@ -96,7 +115,7 @@ function MedicationForm() {
                 <select
                     value={userState.selectValue}
                     onChange={handleChange}
-                    className="form-control" ref={dosage} id="exampleFormControlSelect1">
+                    className="form-control" ref={dosage} id="timesaday">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -107,7 +126,7 @@ function MedicationForm() {
             <div className="form-group">
                 <label htmlFor="exampleFormControlInput1">What time do you want to start taking it?</label>
 
-                <TimeDives ref={log} selectValue={userState.selectValue} />
+                <TimeDives ref={log} handleSelect={handleSelect} selectValue={userState.selectValue} />
                 {console.log("secondory " + userState.selectValue)}
 
             </div>
--- client/src/components/Nav (afterlogin)/index.js
+++ client/src/components/Nav (afterlogin)/index.js
@@ -1,25 +1,36 @@
 import React, { useState, useContext } from "react";
 // import { useStoreContext } from "../../utils/GlobalState";
 import { Navbar, Nav, Button } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { Link } from 'react-router-dom'
 import { StoreContext } from "../../utils/GlobalState"
+import { Router, Route} from 'react-router'
 
 function NavAfterLogin() {
 
   return (
-    <Navbar className="nav-color" expand="lg">
-      <Navbar.Brand href="/dashboard"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo" /></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link className="text-light" href="/dashboard">Dashboard</Nav.Link>
-          <Nav.Link className="text-light" href="/mymedications" >My Medications</Nav.Link>
-          <Nav.Link className="text-light" href="/addmedications">Add Medications</Nav.Link>
-          <Nav.Link className="text-light" href="/home">Logout</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+
+    <div>
+      <ul>
+        <li><Link to = "/dashboard">Dashboard</Link></li>
+        <li><Link to = "/mymedications">My Medications</Link></li>
+        <li><Link to = "/addmedications">Add Medications</Link></li>
+        <li><Link to = "/home">Logout</Link></li>
+      </ul>
+    </div>
+    
+    // <Navbar className="nav-color" expand="lg">
+    //   <Navbar.Brand href="/dashboard"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo" /></Navbar.Brand>
+    //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //   <Navbar.Collapse id="basic-navbar-nav">
+    //     <Nav className="mr-auto">
+    //       <Nav.Link className="text-light" href="/#dashboard">Dashboard</Nav.Link>
+    //       <Nav.Link className="text-light" href="/#mymedications" >My Medications</Nav.Link>
+    //       <Nav.Link className="text-light" href="/#addmedications">Add Medications</Nav.Link>
+    //       <Nav.Link className="text-light" href="/#home">Logout</Nav.Link>
+    //     </Nav>
+    //   </Navbar.Collapse>
+    // </Navbar>
   )
 }
 
