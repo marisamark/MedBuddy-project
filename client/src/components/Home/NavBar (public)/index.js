f2bf76ea142b7d49438dbba719ac949ff1cad068
Emi Rezaei <tomoyose.emi@gmail.com>
2019-12-02 20:55:38 -0800

    added styling nav public

--- client/src/components/Home/NavBar (public)/index.js
+++ client/src/components/Home/NavBar (public)/index.js
@@ -8,9 +8,9 @@ function NavBeforeLogin() {
   <Navbar.Brand href="/home"><img height="70px" width="130px" src="medbuddylogo.png" alt="logo"/></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
+    <Nav className="mr-auto" id="navpublic">
       <Nav.Link className="text-light" href="/health" id="healthandwellness">Health and Wellness</Nav.Link>
-      <Nav.Link className="text-light" href="/signup">Signup</Nav.Link>
+      <Nav.Link className="text-light" href="/signup"style={{paddingLeft: '50px'}}>Signup</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
