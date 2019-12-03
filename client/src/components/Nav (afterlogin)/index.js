a75d0b8dbd2c0e68154857ad6fd065ca19adaf9a
Emi Rezaei <tomoyose.emi@gmail.com>
2019-12-02 23:17:09 -0800

    fixed navbar login

--- client/src/components/Nav (afterlogin)/index.js
+++ client/src/components/Nav (afterlogin)/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 // import { useStoreContext } from "../../utils/GlobalState";
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav, Grid, Row, Col } from 'react-bootstrap';
 
 function NavAfterLogin() {
   return (
+
 <Navbar  className="nav-color" expand="lg">
-  <Navbar.Brand href="/dashboard"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo"/></Navbar.Brand>
+  <Navbar.Brand href="/dashboard" className="col"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo"/></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link className="text-light" href="/dashboard">Dashboard</Nav.Link>
-      <Nav.Link className="text-light" href="/mymedications">My Medications</Nav.Link>
-      <Nav.Link className="text-light" href="/addmedications">Add Medications</Nav.Link>    
-      <Nav.Link className="text-light" href="/home">Logout</Nav.Link>
+    <Nav className="mr-auto" id="nav-loging"> 
+        <Nav.Link className="text-light col" href="/dashboard">Dashboard</Nav.Link>   
+        <Nav.Link className="text-light col-md-auto" href="/mymedications">My Medications</Nav.Link>  
+        <Nav.Link className="text-light col-md-auto" href="/addmedications">Add Medications</Nav.Link>               
     </Nav>
-  </Navbar.Collapse>
+    <Nav.Link className="text-light" href="/home" id="logout">Logout</Nav.Link> 
+  </Navbar.Collapse> 
+     
 </Navbar>
   );
 }
