22a999fac5f99de5c214ac0a025c9a6b3eff9fe1
anthony soon <anthonychansoon@gmail.com>
2019-12-03 10:03:32 -0800

    dont push

--- client/src/components/Nav (afterlogin)/index.js
+++ client/src/components/Nav (afterlogin)/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useContext } from "react";
 // import { useStoreContext } from "../../utils/GlobalState";
 
-import { Navbar, Nav, Button } from 'react-bootstrap';
+// import { Navbar, Nav, Button } from 'react-bootstrap';
 // import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { Link } from 'react-router-dom'
 import { StoreContext } from "../../utils/GlobalState"
-import { Router, Route} from 'react-router'
+import { Router, Route } from 'react-router'
 
 import { Navbar, Nav, Grid, Row, Col } from 'react-bootstrap';
 
@@ -17,13 +17,13 @@ function NavAfterLogin() {
 
     <div>
       <ul>
-        <li><Link to = "/dashboard">Dashboard</Link></li>
-        <li><Link to = "/mymedications">My Medications</Link></li>
-        <li><Link to = "/addmedications">Add Medications</Link></li>
-        <li><Link to = "/home">Logout</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/mymedications">My Medications</Link></li>
+        <li><Link to="/addmedications">Add Medications</Link></li>
+        <li><Link to="/home">Logout</Link></li>
       </ul>
     </div>
-    
+
     // <Navbar className="nav-color" expand="lg">
     //   <Navbar.Brand href="/dashboard"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo" /></Navbar.Brand>
     //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -37,21 +37,6 @@ function NavAfterLogin() {
     //   </Navbar.Collapse>
     // </Navbar>
   )
-
-<Navbar  className="nav-color" expand="lg">
-  <Navbar.Brand href="/dashboard" className="col"><img height="60px" width="120px" src="medbuddylogo.png" alt="logo"/></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto" id="nav-loging"> 
-        <Nav.Link className="text-light col" href="/dashboard">Dashboard</Nav.Link>   
-        <Nav.Link className="text-light col-md-auto" href="/mymedications">My Medications</Nav.Link>  
-        <Nav.Link className="text-light col-md-auto" href="/addmedications">Add Medications</Nav.Link>               
-    </Nav>
-    <Nav.Link className="text-light" href="/home" id="logout">Logout</Nav.Link> 
-  </Navbar.Collapse> 
-     
-</Navbar>
-  );
 
 }
 
